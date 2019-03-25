51a227de1917d6083961cde882c7b1c29e2eb779
Krystian Chachuła <krystiancha@gmail.com>
2019-03-25 22:48:36 +0100

    add final state check

--- blocks_world/blocks.py
+++ blocks_world/blocks.py
@@ -7,16 +7,19 @@ from typing import List, Callable
 class State:
     BLOCK_PADDING = 2
     TABLE_PADDING = 1
+    FINAL_TEXT = "FINAL"
 
     STACKS_TYPE = List[List[int]]
     FINAL_CHECK_TYPE = Callable[[STACKS_TYPE], bool]
     HEURISTIC_TYPE = Callable[[STACKS_TYPE], int]
     HEURISTICS_TYPE = List[Callable[[STACKS_TYPE], int]]
 
+    final_check: FINAL_CHECK_TYPE
     heuristics: HEURISTICS_TYPE
     stacks: STACKS_TYPE
 
-    def __init__(self, heuristics: HEURISTICS_TYPE, layout: STACKS_TYPE = None):
+    def __init__(self, final_check: FINAL_CHECK_TYPE, heuristics: HEURISTICS_TYPE, layout: STACKS_TYPE = None):
+        self.final_check = final_check
         self.heuristics = heuristics
 
         if layout is None:
@@ -33,14 +36,20 @@ class State:
                 reversed(range(max_stack_height)),
                 "",
             )] + [
-                "-" for _ in range(len(self.stacks) * (max_digits + self.BLOCK_PADDING) + self.TABLE_PADDING)
+                "-" for _ in range(
+                    max(
+                        len(self.stacks) * (max_digits + self.BLOCK_PADDING) + self.TABLE_PADDING,
+                        len(self.FINAL_TEXT) + 2 * self.TABLE_PADDING,
+                    )
+                )
             ] + [
-                "\n"
+                ("\n" + "".join(" " for _ in range(self.TABLE_PADDING)) + self.FINAL_TEXT + "\n")
+                if self.is_final() else "\n"
             ])
 
     def heuristic(self):
         """
-        Calculates the value of the heuristic function for a state.
+        Calculates the value of the heuristic function for the state.
         """
         # TODO: take all heuristics into account, not just the first
         return self.heuristics[0](self.stacks)
@@ -61,13 +70,13 @@ class State:
 
     def copy(self):
         """
-        Makes a deep copy of a state.
+        Makes a deep copy of the state.
         """
-        return State(self.heuristics, deepcopy(self.stacks))
+        return State(self.final_check, self.heuristics, deepcopy(self.stacks))
 
     def sprout(self):
         """
-        Makes a list of every possible state, resulting from a state.
+        Makes a list of every possible state, resulting from the state.
         """
         states = []
 
@@ -81,6 +90,12 @@ class State:
 
         return states
 
+    def is_final(self):
+        """
+        Checks if the state is final by executing its final_check function.
+        """
+        return self.final_check(self.stacks)
+
     def _level_to_str(self, level, max_digits):
         return "".join(str(stack[level] if level < len(stack) else "")
                        .rjust(max_digits + self.BLOCK_PADDING) for stack in self.stacks)
@@ -88,3 +103,10 @@ class State:
 
 def blocks_outside_first_stack(stacks: State.STACKS_TYPE) -> int:
     return sum(len(stack) for stack in stacks) - len(stacks[0])
+
+
+def are_blocks_sorted_on_one_stack(stacks: State.STACKS_TYPE, reverse=False) -> bool:
+    if len(stacks) != 1:
+        return False
+    stack = stacks[0]
+    return stack == sorted(stack, reverse=reverse)
--- blocks_world/example.py
+++ blocks_world/example.py
@@ -1,7 +1,11 @@
-from blocks_world.blocks import State, blocks_outside_first_stack
+from blocks_world.blocks import State, blocks_outside_first_stack, are_blocks_sorted_on_one_stack
 
 if __name__ == '__main__':
-    s = State([blocks_outside_first_stack], [[1, 2, 5], [3, 4], [6]])
+    s = State(
+        final_check=are_blocks_sorted_on_one_stack,
+        heuristics=[blocks_outside_first_stack],
+        layout=[[1, 2, 5], [3, 4], [6]],
+    )
     print(s, end="".join(["=" for _ in range(16)] + ["\n\n"]))
     for state in s.sprout():
         print(state)
