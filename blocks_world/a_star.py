5736e51b8c28902c30ae208e7896dfed4b4560f8
wojtasRobert <283234@pw.edu.pl>
2019-04-04 19:38:52 +0200

    A*, needs adjustments

---
+++ blocks_world/a_star.py
@@ -0,0 +1,29 @@
+from blocks_world.blocks import State, blocks_outside_first_stack, are_blocks_sorted_on_one_stack, blocks_sorted_on_first_stack
+
+if __name__ == '__main__':
+    h = []
+    i = 0
+    s = State(
+        final_check=are_blocks_sorted_on_one_stack,
+        heuristics=[blocks_outside_first_stack, blocks_sorted_on_first_stack],
+        layout=[[1, 2, 5], [3, 4, 8, 7], [6, 9]],
+    )
+    print(s, end="".join(["=" for _ in range(16)] + ["\n\n"]))
+
+    G = [s]
+    while not s.is_final():
+
+        P = s.sprout()
+        G.extend(P)
+        G.remove(s)
+        i = i + 1
+
+        for state in G:
+            h.append(state.heuristic())
+
+        m = min(h)
+        m = h.index(m)
+        s = G[m]
+        h.clear()
+        print('iteracja: ', i, '\n')
+        print(s)
