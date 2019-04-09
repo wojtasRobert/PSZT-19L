8f82c7f0c6cd33f639dc604cc46faee3219e9071
Krystian Chachuła <krystiancha@gmail.com>
2019-04-09 23:24:15 +0200

    add a_star tests

--- blocks_world/test_blocks.py
+++ blocks_world/test_blocks.py
@@ -1,6 +1,8 @@
+from functools import reduce
 from unittest import TestCase
 
-from .blocks import State
+from .blocks import State, best_heuristic_ever, misplaced_blocks
+from .a_star import a_star
 
 
 class TestBlocks(TestCase):
@@ -25,3 +27,21 @@ class TestBlocks(TestCase):
 
         state.move(1, 0)
         self.assertEqual([[1, 2, 3, 6], [4, 5]], state.stacks)
+
+
+class TestAStar(TestCase):
+    def test_simple(self):
+        self._test_layout([[0]])
+        self._test_layout([[0, 1, 2, 3]])
+        self._test_layout([[3, 2, 1, 0]])
+
+    def _test_layout(self, layout):
+        initial = State(
+            layout=layout,
+            heuristics=[misplaced_blocks, best_heuristic_ever],
+        )
+        final = State([sorted(reduce(lambda a, b: a + b, initial.stacks, []))])
+
+        r_final, _ = a_star(initial, final)
+
+        self.assertTrue(r_final == final)
