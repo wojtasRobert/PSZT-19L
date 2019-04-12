e98897afd800b13a8d5f4e652d08b91b62eca9cc
Krystian Chachuła <krystiancha@gmail.com>
2019-04-12 14:56:02 +0200

    fix a_star finding non-optimal solutions

--- blocks_world/a_star.py
+++ blocks_world/a_star.py
@@ -7,28 +7,35 @@ class TooManyIterations(Exception):
 
 def a_star(state, final, max_iterations=300, step=False):
     open_set = []
-    closed_set = []
 
     heappush(open_set, state)
     iterations = 0
 
     while len(open_set) > 0:
-        current_state = heappop(open_set)
-        closed_set.append(current_state)
-
         iterations += 1
+
+        current_state = heappop(open_set)
 
         if step:
             print(current_state)
-            input()
+            # input()
 
         if current_state == final:
             return current_state, iterations
 
+        for child in current_state.sprout():
+            try:
+                existing_child_idx = open_set.index(child)
+                existing_child = open_set[existing_child_idx]
+                # child is in the open set
+                if child.cost < existing_child.cost:
+                    del open_set[existing_child_idx]
+                    heappush(open_set, child)
+            except ValueError:
+                # child is not in the open_set
+                heappush(open_set, child)
+
         if iterations > max_iterations:
             raise TooManyIterations
 
-        for child in current_state.sprout():
-            if child not in closed_set:
-                heappush(open_set, child)
-
+    assert False
